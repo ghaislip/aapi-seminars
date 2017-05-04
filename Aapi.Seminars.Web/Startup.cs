a520d9df068c398fbaaf45d9857bea7175d8cded
Zachary McKinnon <zmckinnon@mcksoftware.com>
2017-05-03 21:09:23 -0500

    Add web project

---
+++ Aapi.Seminars.Web/Startup.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Builder;
+
+namespace Aapi.Seminars.Web
+{
+    public class Startup
+    {
+        public void Configure(IApplicationBuilder app)
+        {
+            app.UseDefaultFiles();
+            app.UseStaticFiles();
+        }
+    }
+}
